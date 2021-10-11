4d13ef511802f27f440f7e0e4cb6e37dd3b9bf07
Hugo Gineste <hugo.gineste1@gmail.com>
2021-10-11 10:24:23 +0200

    Ajout de debruitage median

--- bruit.py
+++ bruit.py
@@ -55,7 +55,7 @@ def Bruitage_couleur_poivre_et_sel(longueur,hauteur):
 def Bruitage_poivre_et_sel(longueur,hauteur):
      for i in range (longueur):
         for j in range(hauteur):  
-            if random.randint(0,5) ==5 :
+            if random.randint(0,100) >=50 :
                 if random.randint(0,1)==1:
                     image.putpixel((i,j),(255))
                 else :
@@ -109,14 +109,37 @@ def Debruitage_Median_Couleur(longueur,hauteur):
 def Debruitage_Convolution(longueur,hauteur):
     valeurPixel=np.asarray(image)
 
+
+def debruitage_Median(longueur,hauteur):
+    valeurPixel=np.asarray(image)
+    for i in range(hauteur):
+        for j in range(longueur):
+                if i==(hauteur-1)and j!=(longueur-1):
+                    valMoy=[valeurPixel[i][j-1],valeurPixel[i][j+1],valeurPixel[i-1][j],valeurPixel[i-1][j-1],valeurPixel[i-1][j+1]]
+                    c=np.median(sorted(valMoy))
+                    valeurPixel[i][j]=c
+                if i==(hauteur-1) and j==(longueur-1):
+                    valMoy=[valeurPixel[i-1][j],valeurPixel[i][j-1],valeurPixel[i-1][j-1]]
+                    c=np.median(sorted(valMoy))
+                    valeurPixel[i][j]=c
+                if j==(longueur-1)and i!=(hauteur-1):
+                    valMoy=[valeurPixel[i-1][j],valeurPixel[i+1][j],valeurPixel[i][j-1],valeurPixel[i-1][j-1],valeurPixel[i+1][j-1]]
+                    c=np.median(sorted(valMoy))
+                    valeurPixel[i][j]=c
+                if i!=(hauteur-1) and j!=(longueur-1):
+                    valMoy=[valeurPixel[i-1][j+1],valeurPixel[i+1][j-1],valeurPixel[i+1][j],valeurPixel[i][j+1],valeurPixel[i][j-1],valeurPixel[i-1][j],valeurPixel[i-1][j-1],valeurPixel[i+1][j+1]]
+                    c=np.median(sorted(valMoy))
+                    valeurPixel[i][j]=c
+    return valeurPixel
+
 def Calcul_Snr(valeurImage,l,h):
     c=int(input(" 1 pour un bruitage poivre et sel \n 2 pour un bruitage additif \n 3 pour un bruitage multiplicatif \n 4 pour le debug \n "))
     if (c==1):
-        valeurImageBruitée=Bruitage_poivre_et_sel(l,h)
+        valeurImageBruitée = Bruitage_poivre_et_sel(l,h)
     if (c==2):
-        valeurImageBruitée =BruitageGaussAdditf(l,h)
+        valeurImageBruitée = BruitageGaussAdditf(l,h)
     if (c==3):
-       valeurImageBruitée= BruitageGaussMulti(l,h)
+       valeurImageBruitée = BruitageGaussMulti(l,h)
     if (c==4):
         image.close()
         image2=Image.open('image1_bruitee_snr_41.8939.png')
@@ -129,7 +152,7 @@ def Calcul_Snr(valeurImage,l,h):
             v1+=np.float64(vtmp)
             vtmp=valeurImageBruitée[i,j]**2
             v2+=np.float64(vtmp)
-    print("p")
+    
     print("v1 = ",v1)
     print("v2 = ",v2)
     print("V1-v2 = ",v1 - v2)  
@@ -147,7 +170,7 @@ while True :
         image= Image.fromarray(BruitageGaussMulti(l,h))
         image.show()
     elif (c=='4'):
-        image=Image.fromarray( DebruitageMedian(l,h))
+        image=Image.fromarray( debruitage_Median(l,h))
         image.show()
     elif(c=='5'):
         Calcul_Snr(valeurImage,l,h)
